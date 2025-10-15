fa92a1015c2659355c481704a818c843c0287765
Kendek <kukola.kende@gmail.com>
2025-10-15 10:01:18 +0200

    Müködik

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,14 +1,27 @@
+import { useContext } from "react"
+import { TileContext } from "../context/TileContext"
 
 
 const Map = () => {
 
     const size = 10
-    
+    const ctx = useContext(TileContext)
+
+    const setMapTile = (e: React.MouseEvent) => {
+        if (!ctx) return
+        if(!ctx.selected) return
+        const tile = (e.target as HTMLDivElement)
+        tile.style.backgroundImage = `url(${ctx.selected.image})`
+        tile.style.backgroundSize = (ctx.selected.columnNumber * 100) +"%"
+        tile.style.backgroundPositionX = ctx.selected.x * -100 + '%'
+        tile.style.backgroundPositionY = ctx.selected.y * -100 + '%'
+    }
+
   return (
     <div className="map"
         style={{gridTemplateColumns: `repeat(${size}, 1fr)` }}
     >
-        { Array(size *size).fill("").map(_ => <div></div>) }
+        { Array(size *size).fill("").map(_ => <div onClick={(e) => setMapTile(e)}></div>) }
     </div>
   )
 }
